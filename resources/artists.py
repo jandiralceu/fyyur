3143287b1c9af78340181827c2b8353273412e99
Jandir A. Cutabiala <me@jandir.co>
2024-02-09 02:47:17 -0300

    feat: create artist

--- resources/artists.py
+++ resources/artists.py
@@ -1,5 +1,7 @@
 from flask import flash, redirect, request, render_template, url_for, Blueprint
+from sqlalchemy.exc import SQLAlchemyError
 
+from db import db
 from forms import ArtistForm
 from models.artist import Artist
 
@@ -45,17 +47,29 @@ def create_artist_form():
 @blp.route('/create', methods=['POST'])
 def create_artist_submission():
     """This function handles the HTTP POST request to create a new artist."""
-    # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
-
-    # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    try:
+        artist = Artist(
+            name=request.form["name"],
+            city=request.form["city"],
+            state=request.form["state"],
+            phone=request.form["phone"],
+            genres=request.form["genres"],
+            image_link=request.form["image_link"],
+            facebook_link=request.form["facebook_link"],
+            website_link=request.form["website_link"],
+            seeking_venue=True if request.form["seeking_venue"] == 'y' else False,
+            seeking_description=request.form["seeking_description"],
+        )
+        
+        db.session.add(artist)
+        db.session.commit()
+        
+        flash('Artist ' + artist.name + ' was successfully listed!')
+    except SQLAlchemyError as error:
+        flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+    
     return render_template('pages/home.html')
     
-
 
 @blp.route('/<string:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id: str):

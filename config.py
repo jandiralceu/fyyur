037a6a428982d9bd3f8cc71c94c82ea83a1a58f1
Jandir A. Cutabiala <me@jandir.co>
2024-02-11 17:12:17 -0300

    feat: config postgres and run migrations

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DEBUG = True
 
 FLASK_ENV = "development"
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.db")
+SQLALCHEMY_DATABASE_URI = "postgresql://docker:docker@localhost:5432/nanodegree"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- models/venue.py
+++ models/venue.py
@@ -4,10 +4,14 @@ class Venue(db.Model):
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False) 
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.Text)
